5af9c96b699a1bbfcdeb516b8c252ab07ca728be
jatv93 <toledojennifer20@gmail.com>
2020-07-31 00:19:22 +0000

    register

--- src/main.py
+++ src/main.py
@@ -9,16 +9,23 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, Role, StaffUser, TeacherUser, StudentUser, Profile
-
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended.jwt_manager import JWTManager
+from flask_jwt_extended.utils import create_access_token, get_jwt_identity
+from flask_jwt_extended.view_decorators import jwt_required
+from datetime import datetime
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'secret-key'
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -73,13 +80,56 @@ def student_users():
 
     return jsonify(response_body), 200
 
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
+    if not email:
+        return jsonify({"msg": "Email is required"}), 400
+    if not password:
+        return jsonify({"msg": "Password is required"}), 400
 
+    student = StudentUser.query.filter_by(email=email).first()
+    if student:
+        return jsonify({"msg": "Email already exists"}), 400
+    
+    student = StudentUser()
+    student.name = request.json.get("name", "")
+    student.lastName = request.json.get("lastName", "")
+    student.email = email
+    student.password = bcrypt.generate_password_hash(password)
+    student.role_id = request.json.get("role_id")
 
+    student.save()
 
+    return jsonify({"success": "Register Successfully"}), 200
 
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
+    if not email:
+        return jsonify({"msg": "Email is required"}), 400
+    if not password:
+        return jsonify({"msg": "Password is required"}), 400
 
+    student = StudentUser.query.filter_by(email=email).first()
+    if not student:
+        return jsonify({"msg": "Email/password incorrect"}), 400
+    
+    if not bcrypt.check_password_hash(student.password, password):
+        return jsonify({"msg": "Email/password incorrect"}), 400
+
+    expires = datetime.timedelta(days=3)
+    
+    data = {
+        "access_token": create_access_token(identity=student.email, expires_delta=expires),
+        "student": student.serialize()
+    }
+   
+    return jsonify({"success": "Log In Successfully", "data": data}), 200
 
 
 # this only runs if `$ python src/main.py` is executed
